6a40bde9e04fa8cbe56a921df08e77356299e9af
0400h <github@0400h.cn>
2021-04-26 00:11:52 +0800

    Add rest source

---
+++ build.sh
@@ -0,0 +1,16 @@
+mkdir thirdparty
+cd thirdparty
+
+git clone https://github.com/msgpack/msgpack-c.git
+cd msgpack-c
+git checkout cpp_master
+
+cd ../../examples
+mkdir build
+
+cd build
+rm -rf *
+cmake ..
+make -j `nproc`
+
+cd ..
